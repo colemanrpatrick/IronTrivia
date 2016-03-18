04451c3e8187c3bff3f277421b435f48d2a31d66
bhare1987 <brandonlhare@gmail.com>
2016-03-18 13:08:35 -0400

    started login stuff

--- public/js/templates.js
+++ public/js/templates.js
@@ -3,5 +3,15 @@ module.exports = {
       '<div class="col-sm-8">',
         '<%=question%>',
       '</div>',
-  ].join('')
+  ].join(''),
+  loginForm: [
+    '<div class="col-sm-6 col-sm-offset-3">',
+      '<form>',
+        '<input name="username" type="text" placeholder="User Name">',
+        '<input name="password" type="password" placeholder="Password">',
+        '<button name="login">Login</button>',
+        '<button name="newUser">New User</button>',
+      '</form>',
+    '</div>'
+  ].join(''),
 }
